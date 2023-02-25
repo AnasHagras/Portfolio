12a63299bad2784fa85a8fb6b200c7d2fa3f9f52
BahaaDagher <bahaa.dagher99@gmail.com>
2023-02-25 04:34:04 +0200

    temp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ function App() {
       <h1>Test Header</h1>
       <p> hello from bahaa dagher </p>
       <p> hello from anas hagras </p>
+      <p> hello from anas hagras </p>
+      <p> hello from anas hagras </p>
     </div>
   );
 }
